566c896f7af65ce65de30beb3f2c137dd0b4f375
Ali <60123581+alitarek0@users.noreply.github.com>
2023-04-17 21:12:49 +0200

    add scheduler header file

--- project-scheduler/Processor.h
+++ project-scheduler/Processor.h
@@ -17,25 +17,25 @@ public:
 	bool virtual isBusy() = 0;
 	void virtual setTimeLeftInQueue(int t) = 0;
 	int virtual GetTimeLeft(){
-	int time=0;
-	if(run)time+=run->getTimeLeft();// Adds the time left for the running process to the total time 
-	LinkedQueue<Process*>TempQ;
-	if(ReadyQ->isEmpty())return time;// If ready Queue is empty the time left for processor is the time left for the running process
-	while(!ReadyQ->isEmpty()){
-		Process*temp;
-		ReadyQ->dequeue(temp);
-		time+=temp->getTimeLeft();
-		TempQ.enqueue(temp);// Enquing Processes into temp Queue then Enquing them again into the Ready Queue
-	}
-	while(!TempQ.isEmpty()){
-		Process *x;
-		TempQ.dequeue(x);
-		ReadyQ->enqueue(x);
-	}
+		int time=0;
+		if(run)time+=run->getTimeLeft();// Adds the time left for the running process to the total time 
+		LinkedQueue<Process*>TempQ;
+		if(ReadyQ->isEmpty())return time;// If ready Queue is empty the time left for processor is the time left for the running process
+		while(!ReadyQ->isEmpty()){
+			Process*temp;
+			ReadyQ->dequeue(temp);
+			time+=temp->getTimeLeft();
+			TempQ.enqueue(temp);// Enquing Processes into temp Queue then Enquing them again into the Ready Queue
+		}
+		while(!TempQ.isEmpty()){
+			Process *x;
+			TempQ.dequeue(x);
+			ReadyQ->enqueue(x);
+		}
 	return time;
 	}
 	Process* getrun(){
-	return run;
+		return run;
 	}
 	void AddToQueue(Process*ToAdd){
 		QueueTimeLeft+=ToAdd->getCPUtime();
---
+++ project-scheduler/Scheduler.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <fstream>
+
+class Scheduler {
+
+};
